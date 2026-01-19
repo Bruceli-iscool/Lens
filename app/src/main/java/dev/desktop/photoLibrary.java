8e99cf718807946352d62d8170a99da49a9e2134
Bruce Li <isanoob1897@outlook.com>
2026-01-19 11:18:18 -0800

    Update photoLibrary.java

--- app/src/main/java/dev/desktop/photoLibrary.java
+++ app/src/main/java/dev/desktop/photoLibrary.java
@@ -12,15 +12,18 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
+
 public class photoLibrary {
     ArrayList<String> photos = new ArrayList<>();
-    String catalogPath ="";
+    String catalogPath = "";
+
     protected photoLibrary(boolean firstInCurrentSession) throws InterruptedException, IOException {
         if (firstInCurrentSession) {
             splashScreen();
             home();
         }
     }
+
     protected void splashScreen() throws InterruptedException {
         // splashScreen
         JFrame splash = new JFrame("Lens");
@@ -33,7 +36,9 @@ public class photoLibrary {
         splash.setVisible(true);
         Thread.sleep(2000);
         splash.dispose();
-    }protected void home() throws IOException {
+    }
+
+    protected void home() throws IOException {
         // asks to open or create a catalog.
         JFrame home = new JFrame("Lens Home");
         JButton newCatalog = new JButton("New Catalog");
@@ -58,7 +63,9 @@ public class photoLibrary {
         });
         home.add(openCatalog);
         home.setVisible(true);
-    } protected void openCatalog(JFrame parent) throws FileNotFoundException {
+    }
+
+    protected void openCatalog(JFrame parent) throws FileNotFoundException {
         // open a catalog
         JFileChooser f = new JFileChooser();
         f.setDialogTitle("Select a Lens Catalog to open.");
@@ -71,46 +78,60 @@ public class photoLibrary {
             read(selectedFile);
             render();
         }
-    } protected void read(File file) throws FileNotFoundException {
+    }
+
+    protected void read(File file) throws FileNotFoundException {
         Scanner s = new Scanner(file);
-        while(s.hasNextLine()) {
+        while (s.hasNextLine()) {
             photos.add(s.nextLine());
         }
-    } protected void render() {
+    }
+
+    protected void render() {
         SwingUtilities.invokeLater(() -> {
-            JFrame library = new JFrame("Lens v1.1: "+catalogPath);
+            JFrame library = new JFrame("Lens v1.1: " + catalogPath);
             library.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
             library.setSize(1600, 900);
             library.setLocationRelativeTo(null);
             JPanel panel = new JPanel(new GridLayout(0, 4, 10, 10));
             panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+            // toolbar to add photos
+            JToolBar c = new JToolBar();
+            JButton newCatalog = new JButton("New Catalog");
+            newCatalog.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    newCatalog();
+                }
+            });
+            JButton addPhoto = new JButton("Add Photo");
+            addPhoto.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    JFileChooser f = new JFileChooser();
+                    f.setDialogTitle("Select an Image to open.");
+                    FileNameExtensionFilter filter = new FileNameExtensionFilter("Image files (*.png, *.jpeg, *.jpg)",
+                            "png", "jpeg", "jpg");
+                    f.setFileFilter(filter);
+                    int result = f.showOpenDialog(library);
+                    if (result == JFileChooser.APPROVE_OPTION) {
+                        File selectedFile = f.getSelectedFile();
+                        String s = selectedFile.getPath();
+                        photos.add(s);
+                        render();
+                    }
+                    rewrite();
+                    System.out.println(photos);
+                }
+            });
+            c.add(addPhoto);
+            // todo fix bugs
+            library.add(c, BorderLayout.NORTH);
             for (String path : photos) {
                 ImageIcon icon = new ImageIcon(path);
                 Image scaled = icon.getImage()
                         .getScaledInstance(300, -1, Image.SCALE_SMOOTH);
-                // toolbar to add photos
-                JToolBar c = new JToolBar();
-                JButton addPhoto = new JButton("Add Photo");
-                addPhoto.addActionListener(new ActionListener() {
-                    @Override
-                    public void actionPerformed(ActionEvent e) {
-                        JFileChooser f = new JFileChooser();
-                        f.setDialogTitle("Select an Image to open.");
-                        FileNameExtensionFilter filter = new FileNameExtensionFilter("Image files (*.png, *.jpeg, *.jpg)", "png", "jpeg", "jpg");
-                        f.setFileFilter(filter);
-                        int result = f.showOpenDialog(library);
-                        if (result == JFileChooser.APPROVE_OPTION) {
-                            File selectedFile = f.getSelectedFile();
-                            String s = selectedFile.getPath();
-                            photos.add(s);
-                            render();
-                            rewrite();
-                        }                        
-                    }
-                });
-                c.add(addPhoto);
-                // todo fix bugs
-                library.add(c, BorderLayout.NORTH);
+
                 JButton button = new JButton(new ImageIcon(scaled));
                 button.setBorder(null);
                 button.setContentAreaFilled(false);
@@ -122,28 +143,29 @@ public class photoLibrary {
                     @Override
                     public void actionPerformed(ActionEvent e) {
                         // view the full size image
-                        //ImageIcon deleteIcon = new ImageIcon(photoLibrary.class.getResource(""));
+                        // ImageIcon deleteIcon = new ImageIcon(photoLibrary.class.getResource(""));
                         JToolBar viewImageToolbar = new JToolBar();
                         JButton deleteButton = new JButton("Remove");
                         viewImageToolbar.add(deleteButton);
-                        JFrame viewImage = new JFrame("Lens: "+path);
+                        JFrame viewImage = new JFrame("Lens: " + path);
                         deleteButton.addActionListener(new ActionListener() {
                             @Override
                             public void actionPerformed(ActionEvent e) {
-                                int result = JOptionPane.showConfirmDialog(null, "Remove Image?","Alert!", JOptionPane.YES_NO_OPTION);
-                                if (result==0) {
+                                int result = JOptionPane.showConfirmDialog(null, "Remove Image?", "Alert!",
+                                        JOptionPane.YES_NO_OPTION);
+                                if (result == 0) {
                                     photos.remove(path);
                                     render();
                                     rewrite();
                                     viewImage.dispose();
-                                } 
+                                }
                             }
                         });
                         ImageIcon fullSizeImage = new ImageIcon(path);
                         ImagePanel im = new ImagePanel(fullSizeImage.getImage());
                         viewImage.add(im);
                         viewImage.setSize(1200, 800);
-                        viewImage.add(viewImageToolbar, BorderLayout.NORTH);    
+                        viewImage.add(viewImageToolbar, BorderLayout.NORTH);
                         viewImage.setResizable(true);
                         viewImage.setLocationRelativeTo(null);
                         viewImage.setVisible(true);
@@ -163,25 +185,29 @@ public class photoLibrary {
     }
     private void rewrite() {
         // rewrite the catalog to reflect changes
-        FileWriter p;
         try {
-            p = new FileWriter(catalogPath, false);
-            p.write("");
-            for (String path:photos) {
-                p.write(path+"\n");
+            FileWriter p = new FileWriter(catalogPath, false);
+            for (String path : photos) {
+                p.write(path + "\n");
             }
+            p.close();
         } catch (IOException e) {
-            // TODO Auto-generated catch block
             e.printStackTrace();
         }
     }
+    protected void newCatalog() {
+        // todo
+    }
 }
+
 class ImagePanel extends JPanel {
     // dynamically rescale image
     private final Image image;
+
     public ImagePanel(Image image) {
         this.image = image;
     }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -189,11 +215,11 @@ class ImagePanel extends JPanel {
         int h = image.getHeight(this);
         int panelW = getWidth();
         int panelH = getHeight();
-        double scale = Math.min((double)panelW/w,(double)panelH/h);
+        double scale = Math.min((double) panelW / w, (double) panelH / h);
         int drawW = (int) (w * scale);
         int drawH = (int) (h * scale);
         int x = (panelW - drawW) / 2;
         int y = (panelH - drawH) / 2;
-        g.drawImage(image, x, y, drawW, drawH, this);        
+        g.drawImage(image, x, y, drawW, drawH, this);
     }
 }
